d6608307d192a0d5ebd3553cd82b7ad50ee14b53
Shafic Kiryowa <shafickiryowa2@gmail.com>
2026-08-16 18:31:06 +0300

    feat: enable dark theme by default

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -110,7 +110,7 @@ function Router() {
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
+      <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
           <OfflineBanner />
